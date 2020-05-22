b987173677cb18c8684ce96ee7b3795acca9a7c5
Azamat <Azamatnamialy@gmail.com>
2020-05-23 02:07:09 +0600

    Timer was added

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,14 @@ import "./slider";
 import modals from './modules/modules';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
+import timer from './modules/timer';
 window.addEventListener('DOMContentLoaded',()=>{
     "use strict";
+    let deadline = '2020-08-24';
+
     modals();
     tabs('.glazing_slider','.glazing_block','.glazing_content','active');
     tabs('.decoration_slider','.no_click','.decoration_content > div > div','after_click');
     forms();
+    timer('.container1',deadline);
 });
